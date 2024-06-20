c68e8760a89e4b2ece8dc51b7fe3a326a58fa028
student <student@M40312>
2024-06-20 17:20:16 +0900

    test

--- algo/src/bj/bj15655.java
+++ algo/src/bj/bj15655.java
@@ -1,10 +1,54 @@
 package bj;
 
+import java.io.*;
+import java.util.*;
+
 public class bj15655 {
+	static int n, m;
+	static int[] a;
+	static int[] b;
+	static boolean[] v;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static StringBuilder sb = new StringBuilder();
 
+	static void perm(int depth, int num) {
+		if (depth == m) {
+			for (int x : b)
+				sb.append(x).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if(!v[i] && a[i] > num/*b에 들어간 숫자보다 크다면~*/) {
+				v[i] = true;
+				b[depth] = a[i];
+				perm(depth+1, b[depth]);
+				v[i] = false;
+			}	
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		a = new int[n];
+		b = new int[m];
+		v = new boolean[n];
+
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(a);
+
+		perm(0,0);
+
+		System.out.println(sb.toString());
 	}
 
 }
---
+++ algo/src/bj/bj9663.java
@@ -0,0 +1,10 @@
+package bj;
+
+public class bj9663 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
